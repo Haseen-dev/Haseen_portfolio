d0b7cfe43e8bdba2fa1f3f661efd7cdb54fb8e9c
Mohamed Haseen <85611579+Haseen-dev@users.noreply.github.com>
2024-11-03 17:46:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Element } from 'react-scroll';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero/Hero';
-import About from './Components/about/About';
+import About from './Components/About/About';
 import Services from './Components/Services/Services';
 import MyWork from './Components/MyWork/MyWork';
 import Contact from './Components/Contact/Contact';
