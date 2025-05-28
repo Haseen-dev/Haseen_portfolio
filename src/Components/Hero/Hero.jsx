940028a0ca64a5538dbbe93bc2caeeeb67c4968c
Mohamed Haseen <85611579+Haseen-dev@users.noreply.github.com>
2025-05-28 10:04:49 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -24,9 +24,7 @@ const Hero = () => {
         a valuable addition to any growth-focused team.
       </p> */}
       <p>
-      with a strong background in building responsive and scalable web applications. I specialize in PHP, Laravel, and the MERN stack (MongoDB, Express, React, Node.js), and have hands-on experience delivering impactful solutions across various domains.
-At VeYE Cloud Solutions, I developed a full-stack ERP system for masjid administration, including subscription and family management modules, SMS payment notifications, and cloud deployment via AWS. Prior to that, at Pixecom, I focused on crafting tailored business software and automating workflows to improve operational efficiency. Earlier, at Wavenet, I enhanced the Operations Assistance Portal using the Yii framework, emphasizing performance and user experience.
-I’m passionate about clean UI/UX, efficient backend systems, and contributing to projects that make a meaningful impact. Currently, I’m open to new opportunities where I can continue to grow and deliver high-quality digital solutions.
+      I specialize in developing scalable, user-focused applications using PHP, Laravel, Yii, and the MERN stack. At VeYE Cloud Solutions, I led the development of a cloud-based ERP system for masjid administration. Previously, I crafted business automation tools at Pixecom and improved internal systems at Wavenet, always with a focus on performance and usability.
       </p>
       <div className="hero-action">
         <div className="hero-connect">
